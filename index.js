d8697577d8ae1033ebd840114dfa80dd68e0a49b
Jeremy Habit <jeremy.habit@gmail.com>
2017-09-22 18:16:17 +0200

    optimization createIndex method

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const fs = require('fs')
 const request = require('request')
 const Indexor = require('./modules/indexor')
 
-let indexor = new Indexor('data/fact.json')
+let indexor = new Indexor('data/fact.json', ["content", "author", "date", "authenticity", "title"])
 console.log(indexor.query('korea'))
--- modules/indexor.js
+++ modules/indexor.js
@@ -2,39 +2,37 @@ const fs = require('fs')
 const elasticlunr = require('elasticlunr')
 
 class indexor {
-  constructor(filePath) {
-    this.data = this.parseData(filePath)
-    this.index = this.createIndex()
-    this.generateDocument()
-  }
 
-
-  parseData(filePath) {
-    return JSON.parse(fs.readFileSync(filePath, 'utf8'))
-  }
-
-  createIndex() {
-    return elasticlunr(function () {
-      this.addField('content');
-      this.addField('author');
-      this.addField('date');
-      this.addField('authenticity');
-      this.addField('title');
-      this.setRef('id');
-    })
-  }
-
-  generateDocument() {
-    for (let i = 0; i < this.data.length; i++) {
-      this.data[i].id = i
-      this.index.addDoc(this.data[i])
+    constructor(filePath, fields) {
+        this.data = this.parseData(filePath)
+        this.index = this.createIndex(fields)
+        this.generateDocument()
     }
-  }
 
-  query (str) {
-    return this.index.search(str)
-  }
+    parseData(filePath) {
+        /* mode sync */
+        return JSON.parse(fs.readFileSync(filePath, 'utf8'))
+    }
 
+    createIndex(fields) {
+        return elasticlunr(function () {
+            for (let fieldName of fields) {
+                this.addField(fieldName);
+            }
+            this.setRef('id');
+        })
+    }
+
+    generateDocument() {
+        for (let i = 0; i < this.data.length; i++) {
+            this.data[i].id = i
+            this.index.addDoc(this.data[i])
+        }
+    }
+
+    query(str) {
+        return this.index.search(str)
+    }
 
 }
 
